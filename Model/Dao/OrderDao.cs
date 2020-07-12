1e5678c3f1cb1d399e55b3dac757b4f323f34a00
NguyenDucTuan92N1 <tuan.nguyen19051996@hcmut.edu.vn>
2020-07-13 01:27:01 +0700

    first commit

---
+++ Model/Dao/OrderDao.cs
@@ -0,0 +1,82 @@
+﻿using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class OrderDao
+    {
+        SmartFoodDbContext db = null;
+        public OrderDao()
+        {
+            db = new SmartFoodDbContext();
+        }
+        public long Insert(Order entity)
+        {
+            db.Order.Add(entity);
+            db.SaveChanges();
+            return entity.OrderID;
+        }
+        public List<Order> ListAllOrder(long vendorID)
+        {
+            var baselineDate = DateTime.Now.AddHours(-10);
+
+            return db.Order.Where(x=>x.CreatedDate > baselineDate && x.Status == false && x.VendorID == vendorID).OrderByDescending(x => x.OrderID).ToList();
+        }
+        public long UpdatePaymentMethod(long orderID)
+        {
+            var order = db.Order.Find(orderID);
+
+            if(order.PaymentMethod == 0)
+            {
+                order.PaymentMethod = 1;
+            }
+            else
+            {
+                order.PaymentMethod = 0;
+            }
+            db.SaveChanges();
+            return orderID;
+        }
+        public long UpdatePaymentStatus(long orderID)
+        {
+            var order = db.Order.Find(orderID);
+
+            if (order.PaymentStatus == 0)
+            {
+                order.PaymentStatus = 1;
+            }
+            else
+            {
+                order.PaymentStatus = 0;
+            }
+            db.SaveChanges();
+            return orderID;
+        }
+        public long UpdateFoodStatus(long orderID)
+        {
+            var order = db.Order.Find(orderID);
+
+            if (order.FoodStatus == 0)
+            {
+                order.FoodStatus = 1;
+            }
+            else
+            {
+                order.FoodStatus = 0;
+            }
+            db.SaveChanges();
+            return orderID;
+        }
+        public long UpdateStatus(long orderID)
+        {
+            var order = db.Order.Find(orderID);
+            order.Status = !order.Status;
+            db.SaveChanges();
+            return orderID;
+        }
+    }
+}
